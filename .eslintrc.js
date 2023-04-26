22dc8c52d9b8575c822453595e511b3ca0496f95
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-26 18:03:13 +0200

    + Eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    "prettier"
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
