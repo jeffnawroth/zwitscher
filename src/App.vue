fe60d61804cc0962a99e43dc42e7370f1c631eec
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-26 16:52:28 +0200

    ^ Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-main> </v-main>
+    <v-main><router-view></router-view></v-main>
   </v-app>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,15 @@
 // Composables
-/*import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [{}];
+const routes = [{
+  path: "/",
+  name: "home",
+  component: () => import('@/views/Home.vue'),
+}];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
-export default router; */
+export default router; 
