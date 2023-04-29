6aa954f50bec556f073571d5fa83642155151191
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-29 15:18:00 +0200

    ^ Refactored landing page #3

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,16 +2,28 @@
   <v-container class="fill-height justify-center">
     <v-row>
       <v-col class="d-flex justify-center">
-        <h1>Willkommen!</h1>
+        <span class="text-h2 font-weight-bold"
+          >Willkommen beim Mikroblogging-Dienst!</span
+        >
       </v-col>
-      <v-col class="d-flex justify-center text-center" cols="12">
-        <h2>
+      <v-col
+        v-if="!store.loggedIn"
+        class="d-flex justify-center text-center"
+        cols="12"
+      >
+        <span class="text-h5">
           Um den Mikroblogging-Dienst zu nutzen, müssen Sie sich
           <router-link :to="{ name: 'login' }"> Anmelden </router-link>
           oder
           <router-link :to="{ name: 'register' }"> Registrieren </router-link>
-        </h2>
+        </span>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import { useAuthenticationStore } from "@/store/authentication";
+
+const store = useAuthenticationStore();
+</script>
