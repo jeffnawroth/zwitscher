0e4d3aab940e83b1d170011aa09836dcee13bc81
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-30 15:13:47 +0200

    + BaseDiscardDialog and BasePasswordInput

---
+++ src/components/BasePasswordInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseInputWithValidation
+    :name="name"
+    :label="label"
+    :type="showPassword ? 'text' : 'password'"
+    :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+    @click:append-inner="showPassword = !showPassword"
+  ></BaseInputWithValidation>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseInputWithValidation from "./BaseInputWithValidation.vue";
+
+const showPassword = ref(false);
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
