c5cd2158a2a39bcb0af5409994e09f4f3435eec3
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-30 12:07:01 +0200

    + Base-select component

---
+++ src/components/BaseSelectWithValidation.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-select
+    v-model="value"
+    :label="label"
+    :error-messages="errors"
+    :clearable="true"
+    :items="items"
+    hide-details="auto"
+    @blur="handleBlur"
+  >
+  </v-select>
+</template>
+
+<script setup lang="ts">
+import { defineProps, toRef } from "vue";
+import { useField } from "vee-validate";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  label: {
+    type: String,
+    required: true,
+  },
+
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+
+const { value, handleBlur, errors } = useField(toRef(props, "name"), undefined);
+</script>
