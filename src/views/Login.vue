5e20af316c21cdc43f24878a45a8b79fa0cb0daf
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-30 15:16:11 +0200

    ^ Refactored Login #1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,20 +7,31 @@
     >
       <v-card width="500" title="Anmelden">
         <v-card-text>
-          <BaseInputWithValidation
-            name="email"
-            label="E-Mail"
-            type="email"
-          ></BaseInputWithValidation>
-          <BaseInputWithValidation
-            name="password"
-            label="Passwort"
-            :type="showPassword ? 'text' : 'password'"
-            :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append-inner="showPassword = !showPassword"
-          ></BaseInputWithValidation>
-          <span> Du hast noch kein Konto? </span>
-          <router-link :to="{ name: 'register' }"> Registrieren </router-link>
+          <v-row>
+            <v-col>
+              <BaseInputWithValidation
+                name="email"
+                label="E-Mail"
+                type="email"
+              ></BaseInputWithValidation>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <BasePasswordInput
+                name="password"
+                label="Passwort"
+              ></BasePasswordInput>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <span> Du hast noch kein Konto? </span>
+              <router-link :to="{ name: 'register' }">
+                Registrieren
+              </router-link>
+            </v-col>
+          </v-row>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -38,8 +49,8 @@ import { Form } from "vee-validate";
 import { object, string, setLocale } from "yup";
 import { useAuthenticationStore } from "@/store/authentication";
 import BaseInputWithValidation from "@/components/BaseInputWithValidation.vue";
+import BasePasswordInput from "@/components/BasePasswordInput.vue";
 import router from "@/router";
-import { ref } from "vue";
 import yupLocaleDe from "@/plugins/yupLocaleDe";
 
 setLocale(yupLocaleDe);
@@ -50,8 +61,6 @@ const validationSchema = object({
   email: string().required().email().label("E-Mail"),
   password: string().required().label("Passwort"),
 });
-
-const showPassword = ref(false);
 
 function submit(values: Object) {
   store.login(values);
