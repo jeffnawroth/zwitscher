208949d549d67365b333a7f9afdd60d2e1b1aabe
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-04-30 15:33:23 +0200

    + User management view (WIP) #7

---
+++ src/views/UserManagement.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="store.users"
+    :sort-by="[{ key: 'username', order: 'asc' }]"
+    class="elevation-1"
+  >
+    <template #top>
+      <v-toolbar flat>
+        <v-toolbar-title>Nutzerverwaltung</v-toolbar-title>
+        <v-spacer />
+        <v-btn variant="tonal" @click="$router.push({ name: 'create-user' })"
+          >Nutzer erstellen</v-btn
+        >
+      </v-toolbar>
+    </template>
+    <template #[`item.actions`]="{}">
+      <v-icon class="me-2"> mdi-pencil</v-icon>
+      <v-icon> mdi-delete </v-icon>
+    </template>
+  </v-data-table>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useUsersStore } from "@/store/users";
+
+const store = useUsersStore();
+
+const headers = ref([
+  { title: "Username", key: "username" },
+  { title: "Vorname", key: "firstName" },
+  { title: "Nachname", key: "lastName" },
+  { title: "E-Mail", key: "email" },
+  { title: "Rolle", key: "role" },
+  { key: "actions" },
+]);
+</script>
