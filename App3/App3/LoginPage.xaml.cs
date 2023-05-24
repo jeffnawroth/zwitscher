9bb8787c1692e2e7a2cd22cdeab1778e3424ee59
Philipp Gros <philipp.gros@student.uni-siegen.de>
2023-05-24 15:50:24 +0200

    App Shell

---
+++ App3/App3/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string username = usernameEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (LoginService.ValidateCredentials(username, password))
+            {
+                await DisplayAlert("Erfolgreich", "Login erfolgreich", "OK");
+                Application.Current.MainPage = new AppShell();
+            }
+            else
+            {
+                await DisplayAlert("Fehler", "Ungültige Anmeldeinformationen", "OK");
+            }
+        }
+    }
+}
---
+++ App3/App3/LoginService.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace App3
+{
+    public static class LoginService
+    {
+        public static bool ValidateCredentials(string username, string password)
+        {
+            // When backend ready do the structure
+            return username == "admin" && password == "password";
+        }
+    }
+}
