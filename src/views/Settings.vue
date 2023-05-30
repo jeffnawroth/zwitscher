328de639f0cd09b735b3df4bdc189b1e1c55ef1f
Jeff Nawroth <jeff.nawroth.effexx.com>
2023-05-30 18:18:52 +0200

    + Delete account dialog #15

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,19 +6,36 @@
     <v-divider></v-divider>
     <v-list-item @click="changeEmailDialog = true">E-Mail ändern</v-list-item>
     <v-divider></v-divider>
-    <v-list-item>Konto löschen</v-list-item>
+    <v-list-item @click="deleteAccountDialog = true">Konto löschen</v-list-item>
   </v-list>
 
   <PasswordDialog v-model="changePasswordDialog"></PasswordDialog>
   <EmailDialog v-model="changeEmailDialog"></EmailDialog>
+  <BaseDeleteDialog
+    v-model="deleteAccountDialog"
+    @cancel="deleteAccountDialog = false"
+    @delete="deleteAccount"
+    >diesen Account</BaseDeleteDialog
+  >
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import PasswordDialog from "@/components/Settings/PasswordDialog.vue";
 import EmailDialog from "@/components/Settings/EmailDialog.vue";
+import BaseDeleteDialog from "@/components/BaseComponents/BaseDeleteDialog.vue";
+import { useUsersStore } from "@/store/users";
+import { useAuthenticationStore } from "@/store/authentication";
 
 const changePasswordDialog = ref(false);
 const changeEmailDialog = ref(false);
 const deleteAccountDialog = ref(false);
+
+const usersStore = useUsersStore();
+const authStore = useAuthenticationStore();
+
+function deleteAccount() {
+  // usersStore.deleteUser();
+  authStore.logout();
+}
 </script>
