82c85fa5551d330742e810f59cdce73383048bb0
ErenC61 <ec_61@hotmail.de>
2023-06-10 11:14:45 +0200

    + Added migration files

---
+++ iva-grp7-backend/Migrations/20230607152921_deleteUserDetailsFromPost.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iva_grp7_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteUserDetailsFromPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
