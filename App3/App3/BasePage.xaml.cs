67693b21f6f1063d689eeacb8305bbc5169ad159
Philipp Gros <philipp.gros@student.uni-siegen.de>
2023-06-24 14:22:25 +0200

    PostingButton in AppShell

---
+++ App3/App3/BasePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
+
+namespace App3
+{
+    public partial class BasePage : ContentPage
+    {
+        public BasePage()
+        {
+            // Erstellen Sie den Button
+            var button = new Button
+            {
+                Text = "Post",
+                BackgroundColor = Color.Transparent,
+                TextColor = Color.White,
+                CornerRadius = 30,
+                WidthRequest = 60,
+                HeightRequest = 60,
+                FontSize = 24,
+                HorizontalOptions = LayoutOptions.End,
+                VerticalOptions = LayoutOptions.End,
+                Margin = new Thickness(20)
+            };
+
+            // Fügen Sie dem Button das entsprechende Symbol hinzu
+            button.ImageSource = "sleep_icon.png";
+
+            // Definieren Sie die Klickereignisbehandlung
+            button.Clicked += OnPostButtonClicked;
+
+            // Fügen Sie den Button zur Content-Seite hinzu
+            Content = new StackLayout
+            {
+                Children = { button }
+            };
+        }
+
+        private async void OnPostButtonClicked(object sender, EventArgs e)
+        {
+            // Öffnen Sie die Posting-Seite als Popup
+            var postingPage = new PostingPopUp();
+            await PopupNavigation.Instance.PushAsync(postingPage);
+        }
+    }
+}
+
---
+++ App3/App3/PostingPopUp.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostingPopUp : PopupPage
+    {
+        public PostingPopUp()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCloseButtonClicked(object sender, EventArgs e)
+        {
+            await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
