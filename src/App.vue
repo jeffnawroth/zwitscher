dd81cf469b19fcb713861c8e31444eca40953981
Jeff Nawroth <j.masse10@gmx.de>
2023-07-10 18:13:16 +0200

    + Search (WIP) #33

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,34 @@
   <v-app>
     <v-app-bar flat density="compact" border>
       <v-app-bar-title>Zwitscher</v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <v-text-field
-      variant="solo-filled"
+
+      <v-autocomplete
+        :items="usersStore.users"
+        item-value="id"
+        item-title="name"
+        variant="solo-filled"
         placeholder="Suche..."
         density="compact"
         flat
         hide-details="auto"
-      ></v-text-field>
+      >
+        <template #item="{ props, item }">
+          <v-list-item
+            v-bind="props"
+            :title="item?.raw?.name!"
+            :subtitle="`@${item?.raw?.username!}`"
+            :to="`/${item.raw?.username}`"
+          >
+            <template #prepend>
+              <v-avatar v-if="!item.raw.avatar" color="grey">
+                <v-icon icon="mdi-account-circle" size="x-large"></v-icon>
+              </v-avatar>
+              <v-avatar v-else :image="generateFileURL(item.raw?.avatar)">
+              </v-avatar>
+            </template>
+          </v-list-item>
+        </template>
+      </v-autocomplete>
 
       <IconWithTooltip
         class="mx-2"
@@ -27,7 +45,7 @@
       <IconWithTooltip
         class="mx-2"
         :icon="authIcon"
-        :text="store.loggedIn ? 'Abmelden' : 'Anmelden'"
+        :text="loggedIn ? 'Abmelden' : 'Anmelden'"
         @click="store.logout"
       >
       </IconWithTooltip>
@@ -48,15 +66,15 @@
           rounded="lg"
         >
         </v-list-item>
-        <div v-if="store.loggedIn">
+        <div v-if="loggedIn">
           <v-list-item
-            :to="`/${store.user?.username}`"
+            :to="`/${user?.username}`"
             title="Profil"
             prepend-icon="mdi-account"
             rounded="lg"
           >
           </v-list-item>
-          <div v-if="store.user?.role != Role.NUMBER_2">
+          <div v-if="user?.role != Role.NUMBER_2">
             <v-list-item
               to="/users"
               title="Benutzerverwaltung"
@@ -80,22 +98,26 @@
           >
           </v-list-item>
 
-          <v-list-item v-if="mdAndDown" variant="tonal" rounded="lg"  prepend-icon="mdi-alpha-z" @click="showDialog = true">
-        </v-list-item>
-        <v-list-item v-else >
-          <v-btn block variant="tonal" @click="showDialog = true"
-          >Zwitschern</v-btn
+          <v-list-item
+            v-if="mdAndDown"
+            variant="tonal"
+            rounded="lg"
+            prepend-icon="mdi-alpha-z"
+            @click="showDialog = true"
           >
-        </v-list-item>
+          </v-list-item>
+          <v-list-item v-else>
+            <v-btn block variant="tonal" @click="showDialog = true"
+              >Zwitschern</v-btn
+            >
+          </v-list-item>
         </div>
       </v-list>
-      <div v-if="store.loggedIn">
-        
-        </div>
+      <div v-if="loggedIn"></div>
     </v-navigation-drawer>
 
     <v-navigation-drawer location="right">
-      <FollowedUsersList v-if="store.loggedIn"></FollowedUsersList>
+      <FollowedUsersList v-if="loggedIn"></FollowedUsersList>
     </v-navigation-drawer>
 
     <NotificationContainer></NotificationContainer>
@@ -121,24 +143,34 @@ import IconWithTooltip from "./components/IconWithTooltip.vue";
 import NotificationContainer from "./components/Notification/NotificationContainer.vue";
 import CreatePostDialog from "./components/Posts/CreatePostDialog.vue";
 import FollowedUsersList from "./components/FollowedUsersList.vue";
+import { storeToRefs } from "pinia";
+import { useUsersStore } from "./store/users";
+import { generateFileURL } from "./helpers";
+import { useRouter } from "vue-router";
 
 const store = useAuthenticationStore();
 const settingsStore = useSettingsStore();
 const { mdAndDown } = useDisplay();
+const { loggedIn, user } = storeToRefs(store);
+const usersStore = useUsersStore();
+const router = useRouter();
 
 const showDialog = ref(false);
 
 const authIcon = computed(() => {
-  return store.loggedIn ? "mdi-logout" : "mdi-login";
+  return loggedIn ? "mdi-logout" : "mdi-login";
 });
 
 onMounted(() => {
+  usersStore.getUsers();
+  //Load and set user-data from local storage if exists
   const userString = localStorage.getItem("user");
   if (userString) {
     const userData = JSON.parse(userString);
     store.setUserData(userData);
   }
 
+  //Load and set theme from local storage if exists
   const theme = localStorage.getItem("theme");
   if (theme) settingsStore.setTheme(theme);
 });
